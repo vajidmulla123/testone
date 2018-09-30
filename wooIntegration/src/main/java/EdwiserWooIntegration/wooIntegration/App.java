14cf70b68cc4bedbbb14768ca716bc99ed20beb9
vajidmulla123 <mullavad@gmail.com>
2018-09-30 16:00:35 +0530

    first files

---
+++ wooIntegration/src/main/java/EdwiserWooIntegration/wooIntegration/App.java
@@ -0,0 +1,13 @@
+package EdwiserWooIntegration.wooIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
